ed0b0aed64dc1f5dc0d5916ea137305ff0b1963c
Dennis <d.collaris@me.com>
2016-05-17 21:12:50 +0200

    Remove parallax

--- theme/static/js/script.js
+++ theme/static/js/script.js
@@ -1,136 +1,40 @@
 /// GENERAL ////////////////////////////////////////////////////////////////////
 
-//Show loader for canvas.
-$(document).ready(function(){
-    $('#loading').addClass('animate');
-});
-
 //Menu link: Apps
 $('header > nav li:nth-of-type(1)').click(function(){
-    if($("body").hasClass("mCustomScrollbar")) {
-        $("body").mCustomScrollbar("scrollTo","#what");
-    } else {
-        $("body").animate({ 
-            scrollTop: ($('section#what').offset().top - 
-                        $('section#what').css('margin-top').replace("px","")+3) 
-        }, {
-            duration: 600, 
-            step: function(){
-                lastScrollY = window.scrollY;
-            }
-        }); 
-    }
+    $("body").animate({ 
+        scrollTop: ($('section#what').offset().top - 
+                    $('section#what').css('margin-top').replace("px","")+3) 
+    }, {
+        duration: 600
+    }); 
     return false;
 });
 
 //Menu link: Team
 $('header > nav li:nth-of-type(2)').click(function(){ 
-    if($("body").hasClass("mCustomScrollbar")) {
-        $("body").mCustomScrollbar("scrollTo","#who");
-    } else {
-        $("body").animate({ 
-            scrollTop: ($('section#who').offset().top - 
-                        $('section#who').css('margin-top').replace("px","")-1) 
-        }, {
-            duration: 800, 
-            step: function(){
-                lastScrollY = window.scrollY;
-            }
-        }); 
-    }
+    $("body").animate({ 
+        scrollTop: ($('section#who').offset().top - 
+                    $('section#who').css('margin-top').replace("px","")-1) 
+    }, {
+        duration: 800
+    }); 
+    return false;
 });
 
 //Menu link: Contact
 $('header > nav li:nth-of-type(3)').click(function(){ 
-    if($("body").hasClass("mCustomScrollbar")) {
-        $("body").mCustomScrollbar("scrollTo","bottom");
-    } else {
-        $("body").animate({ 
-            scrollTop: ($(document).height()-$(window).height()) 
-        }, {
-            duration: 1000, 
-            step:function(){
-                lastScrollY = window.scrollY;
-            }
-        });
-    }
+    $("body").animate({ 
+        scrollTop: ($(document).height()-$(window).height()) 
+    }, {
+        duration: 1000
+    });
+    return false;
 });
 
-
-/// SMOOTH SCROLLING ///////////////////////////////////////////////////////////
-var lastScrollY = 0;
-
-//Enable initial smooth scroll
-$(window).load(function(){
-    if (navigator.userAgent.indexOf('Mac OS X') != -1) {
-        $("body").mCustomScrollbar({
-            theme:"minimal",
-            mouseWheel:{ 
-                deltaFactor: 3
-            },
-            callbacks:{
-                whileScrolling:function(){ 
-                    lastScrollY = -this.mcs.top;
-                }
-            }
-        });
-    }
-});
-
-//analyse scroll to determine high-res scrolling (trackpad/magic mouse)
-//then disable smooth scroll based on that.
-var scroll_raw = [];
-var measure_length = 5;
-var lastMeasured;
-
-function analyseScroll(e, delta){
-    var now = Date.now();
-
-    if(typeof lastMeasured !== 'undefined'){
-        var diff = now - lastMeasured;
-        scroll_raw.push(diff);
-    }
-
-    //when we got 'enough' measurements
-    if(scroll_raw.length > measure_length){
-        //calculate average scroll interval
-        var sum = scroll_raw.reduce(function(a, b) { return a + b; });
-        var avg = sum / scroll_raw.length;
-
-        if (avg < 20 && lastScrollY > 50 && lastScrollY < ($(document).height()-50) && !$("body").hasClass("mCS_destroyed")) {
-            console.log('trackpad detected, compensating..');
-            mixpanel.track('Trackpad compensation');
-            setTimeout(function(){$("body").mCustomScrollbar("destroy");},0);
-            window.scrollTo(0, lastScrollY);
-        }
-        scroll_raw = [];
-    }
-
-    lastMeasured = now;
-}
-
-$(document).bind('mousewheel',function(e, delta){
-    if(!$("body").hasClass("mCustomScrollbar")) {
-        analyseScroll(e, delta);
-    }
-});
-
-$(document).bind('scroll', function(){
-    if(!$("body").hasClass("mCustomScrollbar")) {
-        lastScrollY = window.scrollY;
-    }
-});
-
-
-/// PARALLAX EFFECT ////////////////////////////////////////////////////////////
-var ratio2 = 1;
-
-//load header images
-var repeat = new Image(); repeat.src = 'theme/images/background_repeatable.jpg';
-var middle = new Image(); middle.src = 'theme/images/background_middle.jpg';
-
-function onResize() {
-    if($(document).width() < 650){
+function onResize(){
+    console.log('ran!');
+    if($(document).width() < 900){
         //Ellipsis the blog posts
         $("section#blog ul li article").dotdotdot({
             after: "a",
@@ -141,7 +45,7 @@ function onResize() {
 
         //adapt text-size for mobile
         offset = 400;
-        ratio = ($('body').width()/650);
+        ratio = ($('body').width()/670);
         ratio = 1 + (1-(ratio + (1/ratio))/1.76);
         $('header blockquote').css({
             'font-size': (ratio*30)+"px", 
@@ -150,12 +54,7 @@ function onResize() {
         });
 
         //adapt header height for mobile
-        ratio2 = 1 + (1-(ratio + (1/ratio))/1.76);
-
-        $('canvas').width(($(window).width()*2)+50);
-        $('canvas').height($(window).height()+50);
-        $('canvas').css({'top': -50});
-        $('section#how').css({'margin-top': ((ratio2*420))+"px"});
+        $('section#how').css({'margin-top': ((ratio2*320))+"px"});
 
         //adapt doodles for mobile
         $('header blockquote mark:nth-child(3) .doodle').css({
@@ -164,7 +63,7 @@ function onResize() {
             'width': (118*ratio),
             'height': (60*ratio),
             'margin': (-9*ratio)+'px 0 0 '+(2*ratio)+'px'
-        });
+    });
         $('header blockquote mark:nth-child(5) .doodle').css({
             'background-size': (512)*ratio + 'px ' + (801*ratio) + 'px',
             'background-position': (-240*ratio)+'px ' + (-669*ratio) + 'px',
@@ -186,9 +85,6 @@ function onResize() {
         ratio2 = 1;
 
         $('section#how').css({'margin-top': "400px"});
-        $('canvas').width($(window).width()*2);
-        $('canvas').height($(window).height());
-        $('canvas').css({'top': 0});
 
         $('header blockquote').css({
             'font-size': "30px",
@@ -212,94 +108,9 @@ function onResize() {
             'margin': '-7px 0 0 -2px'
         });
     }
-
-    //resize canvas for correct drawing
-    canvas.width = $(window).width()*2;
-    canvas.height = $(window).height();
-    
-    //make sure parallax images are drawn
-    lastScrollY = lastScrollY-1;
-    parallax();
 }
-
-//Setting vars
-var power = -1250;
-var power_hr = -2100;
-var offset = 200;
-
-//Required vars
-var canvas = $('canvas').get(0); 
-$(canvas).attr("height", $(window).height());
-var context = canvas.getContext("2d");
-
-var prevRelativeY = -1;
-var loopId;
-function parallax() {
-    loopId = requestAnimationFrame(parallax);
-
-    try {
-        var relativeY = lastScrollY / 3000;
-
-        if(relativeY !== prevRelativeY){
-            //Header
-            for (var w = (canvas.width/2)-(repeat.width*2.5); w < canvas.width; w += repeat.width-1) {
-                context.drawImage(repeat, w, pos(0, power, relativeY, 0));
-            }
-
-            context.drawImage(middle, ((canvas.width/2)-(repeat.width/2)), pos(0, power, 
-                relativeY, 0));
-
-            //First hr
-            for (var w = ((canvas.width/2)-offset)-(repeat.width*2); w < canvas.width; w += repeat.width-1) {
-                context.drawImage(repeat, w, pos(480, power_hr, relativeY, 0));
-            }
-
-            //team block
-            if(lastScrollY > $('#who').position().top - $(window).height()){
-                for (var w = ((canvas.width/2)-offset)-(repeat.width*2); w < canvas.width*2; w += (repeat.width*2)) {
-                    context.drawImage(repeat, w, pos(600, power-200, relativeY, 0), 1956, 900);
-                }
-            }
-
-            //Second hr
-            if(lastScrollY > ($('#quotes').position().top-50) - $(window).height()){
-                for (var w = ((canvas.width/2)-offset)-(repeat.width*2); w < canvas.width; w += repeat.width-1) {
-                    context.drawImage(repeat, w, pos(1600, power_hr, relativeY, 0));
-                }
-            }
-
-            prevRelativeY = relativeY;
-
-            //Fade out loader when image is loaded.
-            if (!$('#loading').hasClass('fade')) $('#loading').addClass('fade');
-        }
-    }catch(e){
-        cancelAnimationFrame(loopId);
-        throw e;
-    }
-}
-
-function pos(base, range, relY, offset) {
-    return base + limit(0, 1, relY - offset) * range;
-}
-
-function prefix(obj, prop, value) {
-    var prefs = ['webkit', 'Moz', 'o', 'ms'];
-    for (var pref in prefs) {
-        obj[prefs[pref] + prop] = value;
-    }
-}
-
-function limit(min, max, value) {
-    return Math.max(min, Math.min(max, value));
-}
-
-//fix for breaking safari shit
-$(window).load(function(){
-    setTimeout(onResize, 100);
-});
 window.addEventListener('resize', onResize, false);
-
+onResize();
 
 /// APP SWITCHER ///////////////////////////////////////////////////////////////
 $('section#what ul li').click(function(){
